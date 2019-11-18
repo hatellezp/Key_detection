ded3bb00aee1cea03997eef9616ce689088cf71c
hatellezp <hatellezp@gmail.com>
2019-11-18 14:35:36 +0100

    added mehod to download weights

--- create.py
+++ create.py
@@ -29,6 +29,25 @@ else:
     print(("{} doestn't exists, creating from {}"
            .format(model_result_h5,
                    (settings["configuration"] + model_name + ".cfg"))))
+
+    # if weight_zero_path don't exist, attempt to create them
+    if not os.path.exists(weights_zero_path):
+        print("no weights in {}".format(weights_zero_path))
+        print("attempting to get them")
+        if weights_zero_path in ["model_data/yolov3_zero.weights",
+                                 "model_data/yolov3-tiny_zero.weights"]:
+            prefix = "wget https://pjreddie.com/media/files/"
+            if weights_zero_path == "model_data/yolov3_zero.weights":
+                os.system(prefix + "yolov3.weights")
+                os.system("mv yolov3.weights model_data/yolov3_zero.weights")
+                print("weights succefully got and setup")
+            elif weights_zero_path == "model_data/yolov3-tiny_zero.weights":
+                os.system(prefix + "yolov3-tiny.weights")
+                os.system("mv yolov3-tiny.weights model_data/yolov3-tiny_zero.weights")
+                print("weights succefully got and setup")
+        else:
+            raise Exception("no weights in defined location and no method to get them")
+
     # call on convert._main to create model
     config_path = settings["configuration"] + model_name + ".cfg"
     # weights_path = weights_zero_path
